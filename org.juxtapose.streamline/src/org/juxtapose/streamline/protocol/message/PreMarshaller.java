c5b70c882511543b3bc9c674eab2da3ecd59144e
Pontus Jorgne <pontus.jorgne@aphelion.se>
2012-06-01 15:02:54 +0200

    premarshalling methods

--- org.juxtapose.streamline/src/org/juxtapose/streamline/protocol/message/PreMarshaller.java
+++ org.juxtapose.streamline/src/org/juxtapose/streamline/protocol/message/PreMarshaller.java
@@ -1,12 +1,29 @@
 package org.juxtapose.streamline.protocol.message;
 
+import java.util.Iterator;
 import java.util.Map;
 
+import org.juxtapose.streamline.protocol.message.StreamDataProtocol.BigDecimalEntry;
+import org.juxtapose.streamline.protocol.message.StreamDataProtocol.BooleanEntry;
 import org.juxtapose.streamline.protocol.message.StreamDataProtocol.DataMap;
+import org.juxtapose.streamline.protocol.message.StreamDataProtocol.HashMapEntry;
+import org.juxtapose.streamline.protocol.message.StreamDataProtocol.LongEntry;
 import org.juxtapose.streamline.protocol.message.StreamDataProtocol.Message;
+import org.juxtapose.streamline.protocol.message.StreamDataProtocol.NullEntry;
 import org.juxtapose.streamline.protocol.message.StreamDataProtocol.StringEntry;
 import org.juxtapose.streamline.protocol.message.StreamDataProtocol.StringMap;
 import org.juxtapose.streamline.protocol.message.StreamDataProtocol.SubQueryMessage;
+import org.juxtapose.streamline.protocol.message.StreamDataProtocol.UpdateMessage;
+import org.juxtapose.streamline.util.data.DataType;
+import org.juxtapose.streamline.util.data.DataTypeBigDecimal;
+import org.juxtapose.streamline.util.data.DataTypeBoolean;
+import org.juxtapose.streamline.util.data.DataTypeHashMap;
+import org.juxtapose.streamline.util.data.DataTypeLong;
+import org.juxtapose.streamline.util.data.DataTypeNull;
+import org.juxtapose.streamline.util.data.DataTypeString;
+
+import com.google.protobuf.ByteString;
+import com.trifork.clj_ds.IPersistentMap;
 
 /**
  * @author Pontus Jörgne
@@ -40,6 +57,84 @@ public class PreMarshaller
 		messBuilder.setType( Message.Type.SubQueryMessage );
 			
 		return messBuilder.build();
+	}
+	
+	public static Message createUpdateMessage( int inRef, IPersistentMap<String, DataType<?>> inDataMap)
+	{
+		UpdateMessage.Builder builder = UpdateMessage.newBuilder();
+		builder.setType( inRef );
+		
+		DataMap.Builder dataMapBuilder = DataMap.newBuilder();
+		
+		parseMapValues( inDataMap, dataMapBuilder );
+		builder.setData( dataMapBuilder.build() );
+		
+		
+		Message.Builder messBuilder = Message.newBuilder();
+		messBuilder.setUpdateMessage( builder.build() );
+		messBuilder.setType( Message.Type.SubQueryMessage );
+		
+		return messBuilder.build();
+		
+	}
+	
+	public static void parseMapValues( IPersistentMap<String, DataType<?>> inDataMap, DataMap.Builder inBuilder )
+	{
+		Iterator<Map.Entry<String, DataType<?>>> iterator = inDataMap.iterator();
+		
+		while( iterator.hasNext() )
+		{
+			Map.Entry<String, DataType<?>> entry = iterator.next();
+			
+			if( entry.getValue() instanceof DataTypeString )
+			{
+				StringEntry.Builder strBuilder = StringEntry.newBuilder();
+				strBuilder.setField( entry.getKey() );
+				strBuilder.setData( ((DataTypeString)entry.getValue()).get() );
+				inBuilder.addStringEntries( strBuilder.build() );
+			}
+			else if( entry.getValue() instanceof DataTypeBigDecimal )
+			{
+				DataTypeBigDecimal bd = (DataTypeBigDecimal)entry.getValue();
+				BigDecimalEntry.Builder bdBuilder = BigDecimalEntry.newBuilder();
+				bdBuilder.setField( entry.getKey() );
+				bdBuilder.setScale( bd.get().scale());
+				bdBuilder.setIntBytes( ByteString.copyFrom( bd.get().unscaledValue().toByteArray() ));
+				inBuilder.addBDEntries( bdBuilder.build() );
+			}
+			if( entry.getValue() instanceof DataTypeLong )
+			{
+				LongEntry.Builder longBuilder = LongEntry.newBuilder();
+				longBuilder.setField( entry.getKey() );
+				longBuilder.setData( ((DataTypeLong)entry.getValue()).get() );
+				inBuilder.addLongEntries( longBuilder.build() );
+			}
+			if( entry.getValue() instanceof DataTypeBoolean)
+			{
+				BooleanEntry.Builder booleanBuilder = BooleanEntry.newBuilder();
+				booleanBuilder.setField( entry.getKey() );
+				booleanBuilder.setData( ((DataTypeBoolean)entry.getValue()).get() );
+				inBuilder.addBoolEntries( booleanBuilder.build() );
+			}
+			if( entry.getValue() instanceof DataTypeNull )
+			{
+				NullEntry.Builder nullBuilder = NullEntry.newBuilder();
+				nullBuilder.setField( entry.getKey() );
+				inBuilder.addNullEntries( nullBuilder.build() );
+			}
+			if( entry.getValue() instanceof DataTypeHashMap )
+			{
+				HashMapEntry.Builder dataMapBuilder = HashMapEntry.newBuilder();
+				dataMapBuilder.setField( entry.getKey() );
+				
+				DataTypeHashMap hashMap = (DataTypeHashMap)entry.getValue();
+				DataMap.Builder hashMapBuilder = DataMap.newBuilder();
+				
+				parseMapValues( hashMap.get(), hashMapBuilder );
+				
+				dataMapBuilder.setData( hashMapBuilder.build() );
+			}
+		}
 	}
 	
 	/**
--- org.juxtapose.streamline/src/org/juxtapose/streamline/protocol/message/Test.java
+++ org.juxtapose.streamline/src/org/juxtapose/streamline/protocol/message/Test.java
@@ -1,23 +1,124 @@
 package org.juxtapose.streamline.protocol.message;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 import org.juxtapose.streamline.protocol.message.StreamDataProtocol.BigDecimalEntry;
 import org.juxtapose.streamline.protocol.message.StreamDataProtocol.BooleanEntry;
 import org.juxtapose.streamline.protocol.message.StreamDataProtocol.DataMap;
 import org.juxtapose.streamline.protocol.message.StreamDataProtocol.HashMapEntry;
+import org.juxtapose.streamline.protocol.message.StreamDataProtocol.Message;
 import org.juxtapose.streamline.protocol.message.StreamDataProtocol.NullEntry;
 import org.juxtapose.streamline.protocol.message.StreamDataProtocol.StringEntry;
 import org.juxtapose.streamline.protocol.message.StreamDataProtocol.StringMap;
+import org.juxtapose.streamline.util.data.DataType;
+import org.juxtapose.streamline.util.data.DataTypeBigDecimal;
 
 import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.trifork.clj_ds.IPersistentMap;
+import com.trifork.clj_ds.PersistentHashMap;
 
 public class Test {
 
+	public static Map<String, String> createStringKeyValueMap()
+	{
+		Random rand = new Random();
+		
+		HashMap<String, String> keyValues = new HashMap<String, String>();
+		
+		for( int i = 0; i < 200; i++ )
+		{
+			int random = rand.nextInt(1324);
+
+			String key = "K"+Integer.toString( i );
+			String value = "V"+Integer.toString( random);
+			
+			keyValues.put(key, value);
+		}
+		
+		return keyValues;
+	}
+	
+	public static IPersistentMap<String, DataType<?>> createDataKeyValueMap()
+	{
+		Random rand = new Random();
+		
+		IPersistentMap<String, DataType<?>> keyValues = PersistentHashMap.EMPTY;
+		
+		for( int i = 0; i < 200; i++ )
+		{
+			double random = rand.nextDouble();
+
+			String key = "K"+Integer.toString( i );
+			DataTypeBigDecimal value = new DataTypeBigDecimal( random );
+			
+			keyValues = keyValues.assoc(key, value);
+		}
+		
+		return keyValues;
+	}
+	
+	
+	public static void testPerformance(int inIterations ) throws InvalidProtocolBufferException
+	{
+		String t;
+		
+		long startTime = System.nanoTime();
+		
+		long initTime = 0;
+		
+		long anchor = 0l;
+		
+		long totalByteSize = 0;
+		
+		for( int i = 0; i < inIterations; i++ )
+		{
+			long startInit = System.nanoTime();
+			IPersistentMap<String, DataType<?>> keyValues = createDataKeyValueMap();
+			
+			Message mess = PreMarshaller.createUpdateMessage(1, keyValues);
+			
+			initTime +=( System.nanoTime() - startInit );
+			
+			byte[] bytes = mess.toByteArray();
+			
+			totalByteSize += bytes.length;
+			
+			Message mess2 = Message.parseFrom(bytes);
+			
+			anchor += mess2.getUpdateMessage().getData().getBDEntries(0).getSerializedSize();
+		}
+		
+		long endTime = System.nanoTime();
+		
+		long time = endTime - startTime;
+		time-=initTime;
+		
+		time /= 1000;
+		
+		System.out.println(inIterations+" calculations took "+time+" us, anchor =  "+anchor+" byteSize = "+(totalByteSize / inIterations));
+		
+		time /= inIterations;
+		
+		System.out.println("1 calculations took on average "+time+" us \n");
+	}
+	
 	public static void main( String[] inArgs )
 	{
-//		SubQueryMessage.Builder builder = SubQueryMessage.newBuilder();
-//		
+		
+		try
+		{
+			testPerformance(20000);
+			testPerformance(20000);
+		}
+		catch( Exception e )
+		{
+			e.printStackTrace();
+		}
+		
 //		StringMap.Builder dataBuilder = StringMap.newBuilder();
 //		addStringValue( dataBuilder, "CCY1", "EUR" );
 //		addStringValue( dataBuilder, "CCY2", "SEK" );
